bda697d12115eab170376bb903d579e7e1e13119
SoA-Lee <llsa0927@naver.com>
2021-08-08 13:14:29 +0900

    [moon]GridRecyclerView

--- app/src/main/java/com/example/lighititup_design/HospitalAdapter.kt
+++ app/src/main/java/com/example/lighititup_design/HospitalAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.lighititup_design
 
-class HospitalAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class HospitalAdapter (val hospitalList : ArrayList<Hospitals>): RecyclerView.Adapter<HospitalAdapter.CustomViewHolder1>() {
+    //뷰홀더가 처음 생성될때
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HospitalAdapter.CustomViewHolder1 {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_griditem, parent, false)
+        return CustomViewHolder1(view)
+    }
+
+    //재활용해주는 곳 및 값을 넣어주는 곳
+    override fun onBindViewHolder(holder: HospitalAdapter.CustomViewHolder1, position: Int) {
+        holder.h_Image.setImageResource(hospitalList.get(position).image)
+        holder.h_name.text = hospitalList.get(position).name
+        holder.h_field.text = hospitalList.get(position).field
+        holder.h_time.text = hospitalList.get(position).time
+        holder.h_location.text = hospitalList.get(position).location
+    }
+    //리스트의 갯수를 적어준다
+    override fun getItemCount(): Int {
+        return hospitalList.size
+    }
+
+    //뷰홀더 클래스(음료수처럼 잡아주는 홀더)
+    //이곳에서 파인드뷰아이디로 리스트 아이템에 있는 뷰들을 참조한다.
+    inner class CustomViewHolder1(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val h_Image = itemView.findViewById<ImageView>(R.id.imageView_hospital) //병원 종류
+        val h_name = itemView.findViewById<TextView>(R.id.hospital_name)
+        val h_field = itemView.findViewById<TextView>(R.id.hospital_field)
+        val h_time = itemView.findViewById<TextView>(R.id.hospital_time)
+        val h_location = itemView.findViewById<TextView>(R.id.hospital_location)
+    }
 }
--- app/src/main/java/com/example/lighititup_design/MainActivity2.kt
+++ app/src/main/java/com/example/lighititup_design/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.example.lighititup_design
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main2.*
 import java.util.*
@@ -9,7 +10,9 @@ import java.util.*
 class Main2Activity : AppCompatActivity() {
 
     lateinit var fieldAdapter: FieldAdapter
+    lateinit var hospitalAdapter: HospitalAdapter
     val field = ArrayList<LocationItem>()
+    val hospitals = ArrayList<Hospitals>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,21 @@ class Main2Activity : AppCompatActivity() {
         field.add(LocationItem("정형외과"))
         field.add(LocationItem("치과"))
 
+        hospitals.add(Hospitals(R.drawable.h_image0,"[엠엔비여성의원]","산부인과","평일 09시~20시","인천시 계양구"))
+        hospitals.add(Hospitals(R.drawable.h_image0,"[복음병원]","산부인과","평일 09시~20시","인천시 계양구"))
+        hospitals.add(Hospitals(R.drawable.h_image0,"[김성연의원]","산부인과","평일 08시~20시","인천시 계양구"))
+        hospitals.add(Hospitals(R.drawable.h_image0,"[이소아의원]","산부인과","평일 09시~20시","경기도 고양시"))
+        hospitals.add(Hospitals(R.drawable.h_image0,"[안예린의원]","산부인과","평일 10시~20시","서울시 구로구"))
+        hospitals.add(Hospitals(R.drawable.h_image0,"[한유림의원]","산부인과","평일 08시~20시","서울시 노원구"))
+
         fieldAdapter = FieldAdapter(field)
         recyclerView_main2.adapter = fieldAdapter
         recyclerView_main2.setHasFixedSize(true)//어뎁터에 성능을 위한것
         //레이아웃매니저를 이용해 어뎁터의 방향을 결정
         recyclerView_main2.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        hospitalAdapter = HospitalAdapter(hospitals)
+        recyclerGridView_main2.adapter=hospitalAdapter
+        recyclerGridView_main2.layoutManager=GridLayoutManager(applicationContext,2)
     }
 }
