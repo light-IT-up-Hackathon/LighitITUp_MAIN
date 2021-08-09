1089cbc5b3989d36206b8887ba9622cabc5bc77e
SoA-Lee <llsa0927@naver.com>
2021-08-09 15:35:45 +0900

    [mod] FieldAdapter.kt

--- app/src/main/java/com/example/lighititup_design/Hospitals.kt
+++ app/src/main/java/com/example/lighititup_design/Hospitals.kt
@@ -1,3 +1,8 @@
 package com.example.lighititup_design
 
-class Hospitals (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_san (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_ne (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_pi (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_ebi (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_jung (val image : Int, val name : String, val field : String, val time : String, val location :String)
+class Hospitals_chi (val image : Int, val name : String, val field : String, val time : String, val location :String)
